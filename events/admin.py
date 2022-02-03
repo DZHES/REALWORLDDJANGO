b934d00ce6f3ffb032d83bb3098a2deb38ddc488
Esentur <esentur1997@gmail.com>
2022-02-03 01:26:16 -0900

    Update1

--- events/admin.py
+++ events/admin.py
@@ -13,3 +13,11 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(models.Feature)
 class FeatureAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(models.Enroll)
+class EnrollAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated',)
--- events/models.py
+++ events/models.py
@@ -19,6 +19,9 @@ class Feature(models.Model):
             verbose_name_plural = 'Свойства событий'
             verbose_name = 'Свойство события'
 
+    def __str__(self):
+        return self.title
+
 class Event(models.Model):
     title = models.CharField(max_length=200, default='', verbose_name='Название')
     description = models.TextField(default='', verbose_name='Описание')
@@ -38,4 +41,27 @@ class Event(models.Model):
 class Enroll(models.Model):
     user = models.ForeignKey(User, related_name='enrolls', null=True, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, related_name='events', null=True, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата записи')
+
+
+    class Meta:
+        verbose_name_plural = 'Записи на события'
+        verbose_name = 'Запись на событие'
+
+    def __str__(self):
+        return self.user
+
+class Review(models.Model):
+    user = models.ForeignKey(User, related_name='reviews', null=True, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, related_name='reviews', null=True, on_delete=models.CASCADE)
+    rate = models.PositiveSmallIntegerField(null=True, verbose_name='Оценка пользователя')
+    text = models.TextField(default='', verbose_name='Текст пользователя')
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Отзывы на события'
+        verbose_name = 'Отзыв на событие'
+
+    def __str__(self):
+        return  self.user
