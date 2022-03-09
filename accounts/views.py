68b54d389b17f9d439ee7f50f1d41e2032b146b5
Esentur <esentur1997@gmail.com>
2022-03-08 21:07:02 -0900

    Create profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth import authenticate, login
 from accounts.forms import CustomUserCreationForm
 
 
@@ -10,3 +10,12 @@ class CustomSignUpView(CreateView):
     template_name = 'accounts/registration/signup.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('events:event_list')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(self.request, username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+        return result
