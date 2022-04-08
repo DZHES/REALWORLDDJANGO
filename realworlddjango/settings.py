6c9bc75ded0d2c77f1c55ccc5613701626aa4274
Esentur <esentur1997@gmail.com>
2022-04-07 16:57:37 -0900

    Add static settings

--- realworlddjango/settings.py
+++ realworlddjango/settings.py
@@ -6,16 +6,16 @@ from .settings_base import *
 
 DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DB_NAME'),
-#         'USER': env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST'),
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '',
+    }
+}
 
 sentry_sdk.init(
     dsn=env('dsn'),
